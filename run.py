762b9406a6ddf0318b545248b54b6f09f723ada1
Agustin Muniz <gus.s.muniz@gmail.com>
2015-07-27 16:48:11 -0700

    added key checks before accessing data dictionary

--- run.py
+++ run.py
@@ -14,14 +14,17 @@ def run():
 
 	# queue up fetch objects
 	fetch_list = []
-	for obj in jsonobj['ftp']:
-		fetch_list.append(FtpFetch(obj['site'], obj['collection'], obj['file_name_root'], obj['save_dir'], obj['exts']))
+	if 'ftp' in jsonobj:
+		for obj in jsonobj['ftp']:
+			fetch_list.append(FtpFetch(obj['site'], obj['collection'], obj['file_name_root'], obj['save_dir'], obj['exts']))
 
-	for obj in jsonobj['usgs']:
-		fetch_list.append(USGSFetch(obj['modis_terra'], obj['collection'], obj['save_dir'], obj['min_year'], obj['exts']))
+	if 'usgs' in jsonobj:
+		for obj in jsonobj['usgs']:
+			fetch_list.append(USGSFetch(obj['modis_terra'], obj['collection'], obj['save_dir'], obj['min_year'], obj['exts']))
 
-	for obj in jsonobj['nersc']:
-		fetch_list.append(NERSCFetch(obj['collection'], obj['save_dir'], obj['min_year'], obj['exts']))
+	if 'nersc' in jsonobj:ß
+		for obj in jsonobj['nersc']:
+			fetch_list.append(NERSCFetch(obj['collection'], obj['save_dir'], obj['min_year'], obj['exts']))
 
 	#fetch
 	logs = []
@@ -32,12 +35,12 @@ def run():
 	#validate logs
 	logV = WgetLogValidator()
 	logV.validate_logs(logs)
-	
+
 	subject = 'Daily Download Summary'
 	text = logV.summary_str()
-	
+
 	mailObj = GmailSend(config.emailusr(), config.emailpwd(), config.emailusr(), subject, text, config.emaillist())
 	mailObj.send_email()
-	
+
 if __name__ == "__main__":
 	run()
